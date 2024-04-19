b8f49808c8b7ac515d53fcf7dab5812ddbe4cc98
ShexZ <116831826+agabagbum@users.noreply.github.com>
2024-04-19 14:13:51 +0200

    second commit

---
+++ Pogodynka/src/api.jsx
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "328dc10370msh789579b0eec95d9p1ae860jsn5bf1a9d620d5",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "432a5fdc008725acc44ea2320af70a2e";
---
+++ Pogodynka/src/components/SearchCity/search.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiOptions, GEO_API_URL } from "../../api";
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+export default Search;
